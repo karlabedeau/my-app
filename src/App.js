2641ed89a7ad367dfa4b3b956ebe6a639ac29096
karlabedeau <67243358+karlabedeau@users.noreply.github.com>
2021-02-07 14:51:55 -0500

    Added React integration from Codesandbox to VSC

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Axios from 'axios';
+import "./styles.css";
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+        <div class="vanilla-weather-app">
+          <div class="card">
+            <div class="card-body">
+              <h1 id="city">#</h1>
+              <ul class="date-and-time">
+                <li id="time"></li>
+                <li id="description"></li>
+              </ul>
+              <br />
+              <form class="weather-app" id="search-form">
+                <div class="row">
+                  <input
+                    type="text"
+                    placeholder="Enter city name here"
+                    class="form-control col-9"
+                    id="city-input"
+                  />
+                  <input
+                    type="submit"
+                    value="Search"
+                    class="form-control col-3 btn btn-primary"
+                  />
+                </div>
+              </form>
+              <br />
+              <br />
+
+              <div class="card">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-xl current-sky">
+                      <img src="#" alt="cloudy" id="icon" />
+                      <span class="current-temperature"></span>
+                      <span class="units">°C</span>
+
+                      <div class="col-12">
+                        <forms class="float-right">
+                          <ul class="weather-percentage">
+                            <li>
+                              Humidity: <span id="humidity"></span>%
+                            </li>
+                            <li>
+                              Wind: <span id="wind"></span>km/h
+                            </li>
+                          </ul>
+                        </forms>
+                        <p class="ending-remark">Have a nice Day!</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <img
+                class="seasons"
+                src="images/season_change.png"
+                alt=""
+                width="100"
+              ></img>
+              <p class="ending-remark">Have a nice Day!</p>
+
+              <br />
+              <p class="every">Forecast every 3 hours</p>
+
+              <div class="row weather-forecast" id="forecast"></div>
+              <div class="footer">
+                This
+                <a
+                  class="open"
+                  href="https://github.com/karlabedeau/My-Weather-Forecast"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Open-source code
+                </a>
+                and
+                <a
+                  href="https://peaceful-liskov-c2f7e7.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Hosted on Netlify
+                </a>
+                weather report was build by Karla Bedeau
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
