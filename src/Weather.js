52d96ed62dd06af9233cb2d8da5e908b556abe5a
karlabedeau <67243358+karlabedeau@users.noreply.github.com>
2021-03-08 21:19:50 -0500

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import axios from 'axios';
 import "./Weather.css";
 import Loader from "react-loader-spinner";
