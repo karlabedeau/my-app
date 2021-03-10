8b8bfbd6e32f99fa815da56f0bf5f9f5e4391abb
karlabedeau <67243358+karlabedeau@users.noreply.github.com>
2021-03-10 17:02:29 -0500

    Search engine

--- src/Weather.js
+++ src/Weather.js
@@ -6,8 +6,8 @@ import Loader from "react-loader-spinner";
 
 export default function App(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
   function handleResponse(response) {
-    console.log(response.data);
     setWeatherData({
       ready: true,
       temperature: response.data.main.temp,
@@ -18,6 +18,21 @@ export default function App(props) {
       wind: response.data.wind.speed,
       city: response.data.name
     });
+  }
+  
+  function search() {
+    const apiKey = "cc18170596dd9fcd6f13e84c39a52c30";
+    let apiURL =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiURL).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
   }
 
   if (weatherData.ready) {
@@ -34,13 +49,14 @@ export default function App(props) {
                 <li className="text-capitalize" id="description">{weatherData.description}</li>
               </ul>
               <br />
-              <form className="weather-app" id="search-form">
+              <form onSubmit={handlesubmit} className="weather-app" id="search-form">
                 <div className="row">
                   <input
                     type="text"
                     placeholder="Enter city name here"
                     className="form-control col-9"
                     autoFocus="on"
+                    onChange={handleCityChange}
                     id="city-input"
                   />
                   <input
@@ -87,7 +103,7 @@ export default function App(props) {
               <p className="ending-remark">Have a nice Day!</p>
 
               <br />
-              <p className="every">Forecast every 3 hours</p>
+              <p className="every">Forecast of the Day</p>
 
               <div className="row weather-forecast" id="forecast"></div>
             </div>
@@ -96,10 +112,7 @@ export default function App(props) {
     </div>
   );
   } else {
-    const apiKey = "cc18170596dd9fcd6f13e84c39a52c30";
-    let apiURL =`https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiURL).then(handleResponse);
-
+    search();
     return <Loader type="Oval" color="purple" height={80} width={80} />;
   } 
 }
