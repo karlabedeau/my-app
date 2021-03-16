a5b17daccd5e4c26fff637d999224b3f9cf378d3
karlabedeau <67243358+karlabedeau@users.noreply.github.com>
2021-03-15 20:45:08 -0400

    Adding icon when weather changes

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -2,12 +2,44 @@ import React from "react";
 import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherIcon(props) {
-    return (
-        <ReactAnimatedWeather
-            icon={defaults.icon}
-            color={defaults.color}
-            size={defaults.size}
-            animated={defaults.animate} 
-        />
-    );
+    const codeMapping = {
+        "01d": "CLEAR_DAY",
+        "01n": "CLEAR_NIGHT",
+        "02d": "PARTLY_CLOUDY_DAY",
+        "02n": "PARTLY_CLOUDY_NIGHT",
+        "03d": "PARTLY_CLOUDY_DAY",
+        "03n": "PARTLY_CLOUDY_NIGHT",
+        "04d": "CLOUDY",
+        "04n": "CLOUDY",
+        "09d": "RAIN",
+        "09n": "RAIN",
+        "10d": "RAIN",
+        "10n": "RAIN",
+        "11d": "RAIN",
+        "11n": "RAIN",
+        "13d": "SNOW",
+        "13n": "SNOW",
+        "50d": "FOG",
+        "50n": "FOG"
+    }
+
+    if (props.code === "01d") {
+        return (
+            <ReactAnimatedWeather
+                icon="CLEAR_DAY"
+                color="1e1e1e"
+                size={64}
+                animated={true} 
+            />
+        );
+    } else {
+        return (
+            <ReactAnimatedWeather
+                icon="CLEAR_NIGHT"
+                color="1e1e1e"
+                size={64}
+                animated={true} 
+            />
+        );
+    }
 }
